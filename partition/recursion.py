9814c1ad09300ceaa79cad99297692017179df29
Tristan Guigue <tristan@tyba.com>
2015-04-15 17:08:24 +0200

    Remove number of partitions from partition problem

--- partition/recursion.py
+++ partition/recursion.py
@@ -32,7 +32,7 @@ def cost(word):
         return len(word)
 
 
-def split(input_string, start, number_of_partitions):
+def split(input_string, start):
     """The recrusive function, it tries to split the substring starting at
     `start` into `number_of_partitions` words
 
@@ -40,8 +40,6 @@ def split(input_string, start, number_of_partitions):
         input_string (string): the initial string that needs to be split
         start (int): we want to split the substring of input_string
             starting at that index
-        number_of_partitions (int): the maximum number of partitions we can use
-            to split this substring
 
     Returns:
         A tupple form of the partial solution and its cost
@@ -50,11 +48,7 @@ def split(input_string, start, number_of_partitions):
     # the substring to split
     substring = input_string[start:]
 
-    # Those are the boundary conditions
-    # if we're out of partitions return the substring and its cost
-    if not number_of_partitions:
-        return substring, cost(substring)
-
+    # This is the boundary conditions
     # if the substring is empty return an empty string with no cost
     if not len(substring):
         return '', 0
@@ -64,28 +58,31 @@ def split(input_string, start, number_of_partitions):
 
     # we place our next partition somewhere between start and the end of the
     # input_string
-    for i in range(0, len(substring) + 1):
+    for i in range(1, len(substring) + 1):
         # we split the rest of the string recursively
-        rest_string, rest_cost = split(input_string, start + i,
-                                       number_of_partitions - 1)
+        rest_string, rest_cost = split(input_string, start + i)
 
         current_string = substring[:i]
         current_cost = cost(current_string) + rest_cost
 
         # update minum cost and string if it's the best so far
         if min_cost is None or current_cost < min_cost:
-            min_cost = current_cost
 
             # if the two parts are not empty join them with space
             if current_string and rest_string:
                 min_string = current_string + ' ' + rest_string
+                # We add a cost for white space to avoid spliting unkown words
+                # into small pieces
+                current_cost += 1
             else:
                 min_string = current_string + rest_string
+
+            min_cost = current_cost
 
     return min_string, min_cost
 
 
 input_string = "carlosisnice"
 
-print split(input_string, 0, len(input_string) - 1)
-# print ('carlos is nice', 6)
+print split(input_string, 0)
+# print ('carlos is nice', 8)
