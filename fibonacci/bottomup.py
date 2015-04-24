fb7754a9d1ac6ee7a62d6d4e3a560f4825fe812b
Tristan Guigue <tristan@tyba.com>
2015-04-24 19:08:25 +0200

    Fix range error

--- fibonacci/bottomup.py
+++ fibonacci/bottomup.py
@@ -9,11 +9,11 @@ def fibonacci(n):
     fn_2 = 0
     fn_1 = 1
 
-    for i in range(0, n):
+    for i in range(2, n + 1):
         fi = fn_1 + fn_2
         fn_1, fn_2 = fi, fn_1
 
     return fi
 
-print fibonacci(45)
-# print 1134903170
+print fibonacci(10)
+# print 55
